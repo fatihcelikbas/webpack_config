2d6bdba3353f3be71c26de8844521f3657353f23
Fatih Celikbas <clkbs.fth@gmail.com>
2021-04-25 11:48:10 -0700

    Simple changes for placeholder @ Netlify

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,4 @@
 // No need to import "react" just for JSX in React 17+
-import Recipes from "./Recipes";
-import sword from "../images/swc-sword.png";
-import swordSvg from "../images/sword.svg";
-
 import "../styles/index.scss";
 
 const App = () => {
@@ -10,13 +6,12 @@ const App = () => {
     <>
       <section className="hero"></section>
       <main>
-        <section>
-          <h1>Oh Herro, React.</h1>
-          <img src={sword} alt="sword" width="250" />
-          <img src={swordSvg} alt="sword" width="250" />
-          <Recipes />
+        <section className="row">
+          <h1>Ramiz And Me!</h1>
+          <p>Current placeholder</p>
         </section>
       </main>
+      <section className="hero"></section>
     </>
   );
 };
