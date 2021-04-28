f7eb081ff98c70c68253e1f186f9a41b5c143e7d
Fatih Celikbas <clkbs.fth@gmail.com>
2021-04-27 20:10:51 -0700

    Add prettier, integrate with esling and format

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,28 +3,28 @@ const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
-	mode: 'production',
-	devtool: false,
-	plugins: [new MiniCssExtractPlugin()],
-	// Temporary workaround for 'browserslist' bug that is being patched in the near future
-	target: 'browserslist',
-	module: {
-		rules: [
-			{
-				test: /\.(s[ac]|c)ss$/i,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						// This is required for asset imports in CSS, such as url()
-						options: { publicPath: '' },
-					},
-					'css-loader',
-					'postcss-loader',
-					// according to the docs, sass-loader should be at the bottom, which
-					// loads it first to avoid prefixes in your sourcemaps and other issues.
-					'sass-loader',
-				],
-			},
-		],
-	}
+  mode: 'production',
+  devtool: false,
+  plugins: [new MiniCssExtractPlugin()],
+  // Temporary workaround for 'browserslist' bug that is being patched in the near future
+  target: 'browserslist',
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url()
+            options: { publicPath: '' },
+          },
+          'css-loader',
+          'postcss-loader',
+          // according to the docs, sass-loader should be at the bottom, which
+          // loads it first to avoid prefixes in your sourcemaps and other issues.
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 });
