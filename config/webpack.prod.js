86da426e3a7d1e1375c94d3a99bb7c00dd63b82b
Fatih Celikbas <clkbs.fth@gmail.com>
2021-04-29 00:39:31 -0700

    Add more optimization to the prod config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,5 +1,7 @@
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -26,5 +28,10 @@ module.exports = merge(common, {
         ],
       },
     ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()],
+    runtimeChunk: 'single',
   },
 });
